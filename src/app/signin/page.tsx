6fc23aabca994b3a23c3ac32fbecbc4779a6e385
tkaykim <tommy0621@naver.com>
2025-07-22 20:36:42 +0900

    Revert "suspend"

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,10 +1,5 @@
-import { Suspense } from 'react'
 import { SigninForm } from '@/components/auth/SigninForm'
 
 export default function SigninPage() {
-  return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <SigninForm />
-    </Suspense>
-  )
+  return <SigninForm />
 } 
